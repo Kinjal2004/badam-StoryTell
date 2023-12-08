d0ca2343bacd82f3a0b18018c72c6fd484010ae1
Kinjal2004 <bkinjal2004@gmail.com>
2023-12-08 13:02:57 +0530

    fixed the createpage

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
-import StoryTextArea from "@/components/StoryTextArea";
-import SubmitButton from "@/components/SubmitButton";
+import CreatePage from "@/components/createComponents/CreatePage";
 
-const CreatePage: React.FC = () => {
-  const [story, setStory] = useState<string>("");
+const Create: React.FC = () =>{
 
-  const handleStoryChange = (value: string) => {
-    setStory(value);
-  };
+    return(
+        <CreatePage />
+    )
+}
 
-  const handleSubmit = () => {
-    console.log("Submitted story:", story);
-  };
-
-  return (
-    <div
-      className="bg-cover bg-center min-h-screen flex justify-center items-center flex-col"
-      style={{
-        backgroundImage:
-          "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-meniscus-fire-cloud-imagination-background-image_2190874.jpg",
-      }}
-    >
-      <h1 className="text-4xl font-bold mb-6">Create Your Story</h1>
-      <StoryTextArea story={story} onStoryChange={handleStoryChange} />
-      <SubmitButton onClick={handleSubmit} />
-    </div>
-  );
-};
-
-export default CreatePage;
+export default Create
