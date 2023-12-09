3fe167fd9022720a5a0253a895112777b1786a57
Arya <bhattacharyyaarya165@gmail.com>
2023-12-09 13:06:44 +0530

    fix:pull issue

--- app/api/createStory/route.tsx
+++ app/api/createStory/route.tsx
@@ -1,16 +1,13 @@
 import prisma from "@/db";
 import { NextResponse } from "next/server";
 import { parse } from "url";
-import { Request } from 'next';
-
-
+import { Request } from "next";
 
 export async function POST(req: Request) {
   try {
-   const data = await req.json();
-   console.log(data.title);
-   return NextResponse.json({ status: 200 });
-
+    const data = await req.json();
+    console.log(data.title);
+    return NextResponse.json({ status: 200 });
   } catch (e) {
     console.log(e);
   }
