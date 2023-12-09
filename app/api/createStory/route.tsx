36ee1b487075321df27fb2409ee224d52e60060b
Arya <bhattacharyyaarya165@gmail.com>
2023-12-09 13:17:53 +0530

    feat:added other console.log

--- app/api/createStory/route.tsx
+++ app/api/createStory/route.tsx
@@ -7,6 +7,8 @@ export async function POST(req: Request) {
   try {
     const data = await req.json();
     console.log(data.title);
+    console.log(data.story);
+    console.log(data.genres);
     return NextResponse.json({ status: 200 });
   } catch (e) {
     console.log(e);
